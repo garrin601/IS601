69ae155ccb164af06e36e04d2ae519abf7248349
garrin c saccento <gcs23@afsconnect2.njit.edu>
2017-09-12 16:40:47 -0400

    php test file

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<?php
+	$class = 'IS601';
+	echo'hello world $class </br>';
+	echo"hello world $class";
+
+
+
+
+$obj = new main();
+
+class main {
+	
+	public function _construct () {
+
+	   echo ' hello i\'m an Object';
+	}
+
+
+	public function _destruct () {
+
+	   echo '</br> I am done';
+	}
+}
+
+?>
