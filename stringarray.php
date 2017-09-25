51c613bf21a745e2c5e8709b12229d630b292d00
garrin c saccento <gcs23@afsconnect2.njit.edu>
2017-09-25 13:03:05 -0400

    10/10 string functions completed

--- stringarray.php
+++ stringarray.php
@@ -1,4 +1,7 @@
 <?php
+
+//Number  1/10
+
 print '<h1> count character string function </h1>'; 
 $test = 'This is just a test'; 
 foreach (count_chars($test, 1) as $i => $val) {
@@ -8,6 +11,7 @@ print '<hr />';
 
 print '<hr />';
 
+// Number 2/10
 
 print '<h1> This is the echo string function </h1>';
 
@@ -19,6 +23,7 @@ echo "I am in your lecture, my name is $name,$lastname";
 
 echo '<hr />'; 
 
+// Number 3/10
 
 print '<h1> This is the explode function </h1>';
 
@@ -29,6 +34,7 @@ echo "I do not like $sodas[2] whatsoever </br>";
 
 echo '<hr />';
 
+// Number 4/10
 
 print '<h1> This is the add slash string function</h1>';
 
@@ -37,6 +43,8 @@ $question = "Hello, how are you today Mr. O'Shea?";
 echo addslashes ($question);
 
 echo '<hr />';
+
+// Number 5/10
 
 print '<h1> This is the chop string function  </h1>';
 
@@ -49,6 +57,7 @@ echo "</pre>";
 
 echo '<hr />';
 
+// Number 6/10
 
 print '<h1> This is the hex2bin string function </h1>';
 
@@ -57,6 +66,75 @@ $hex = hex2bin("68656c6c6f2070726f666573736f722077696c6c69616d73");
 var_dump($hex);
 
 echo '<hr />';
+
+//number 7/10
+
+Print '<h1> This is the join string function </h1>';
+
+$firstarray = array('a', 'b', 'c', 'd', 'e', 'f', 'g');
+
+echo join ('<br>', $firstarray);
+
+echo '<hr />';
+
+// Number 8/10
+
+print '<h1> This is the lcfirst string function </h1>';
+
+
+$example1 = "GARRINSACCENTO";
+$example1[0] = strtolower($example1[0]);
+echo "$example1";
+
+echo '<hr />';
+
+// Number 9/10
+
+print '<h1> This is the metaphone string function </h1>';
+
+
+$metatest1 = 'hunter';
+$metatest2 = 'hunting';
+var_dump(metaphone($metatest1, 5 ));
+print '</br>';
+var_dump(metaphone($metatest2, 5));
+
+echo '<hr />';
+
+
+// Number 10/10
+
+print '<h1> This is the sprintf string function  </h1>';
+
+$number = 10;
+$place = 'offices';
+$format = 'The majority of the company has %d small %s all around New Jersey';
+echo sprintf ($format, $number, $place);
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
